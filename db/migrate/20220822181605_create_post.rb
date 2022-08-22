c7e44b0503333b99e8f23b5974476e59cd80fd38
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-22 19:27:52 +0100

    Create post table and add columns

--- db/migrate/20220822181605_create_post.rb
+++ db/migrate/20220822181605_create_post.rb
@@ -1,6 +1,9 @@
 class CreatePost < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.string :title 
+      t.string :text
+      t.integer :CommentCounter
 
       t.timestamps
     end
