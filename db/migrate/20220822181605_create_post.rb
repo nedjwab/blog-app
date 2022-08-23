048a170ab4f2c9d39bf67bf7394e061c0254356c
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-23 22:20:03 +0100

    Add likes counter in post table

--- db/migrate/20220822181605_create_post.rb
+++ db/migrate/20220822181605_create_post.rb
@@ -4,6 +4,7 @@ class CreatePost < ActiveRecord::Migration[7.0]
       t.string :title
       t.string :text
       t.integer :CommentCounter
+      t.integer :likesCounter
 
       t.timestamps
     end
