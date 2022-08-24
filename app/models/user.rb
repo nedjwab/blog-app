cc9374ef6f027263f82234af1c1d4ccc8033a617
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-24 10:22:40 +0100

    Create recent posts method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :posts
+
+  def user_posts()
+    Post.where(user_id(:userid)).order(created_at: :desc).limit(3)
+  end
 end
