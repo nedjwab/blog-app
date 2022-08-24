249ee7459c8cf8a3e555917011c843c9cb341b32
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-24 11:06:23 +0100

    Add comment method in the posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :posts
 
-  def user_posts()
-    Post.where(user_id(:userid)).order(created_at: :desc).limit(3)
+  def user_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
