15b49fc9203e7393ca0ffcba5ecc6a3603534291
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-25 11:48:10 +0100

    Add title validation for the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+  validates :title, presence: true, length: { maximum: 250 }
 
   after_save :update_counter
 
