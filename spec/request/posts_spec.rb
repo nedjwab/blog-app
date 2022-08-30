59acb7ea838476fb8d8fcbd4b8d3f53d1be0b3ae
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-08-30 21:07:24 +0100

    Add tests for the posts show action

--- spec/request/posts_spec.rb
+++ spec/request/posts_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe 'Posts Controller', type: :request do
       expect(response.body).to include('the given users posts list')
     end
   end
+
+  describe 'The show action' do
+    it 'should return an ok status ' do
+      get '/users/:id/posts/:id'
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return the index template' do
+      get '/users/:id/posts/:id'
+      expect(response).to render_template('show')
+    end
+
+    it 'should return the given post details' do
+      get '/users/:id/posts/:id'
+      expect(response.body).to include('the given post details')
+    end
+  end
 end
