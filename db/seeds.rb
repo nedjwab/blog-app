41fce35db34840c0858aebb60b22a57038b49213
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-01 15:15:44 +0100

    Add some data for the user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: "Luke", movie: movies.first)
 first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my first post')
+first_post = Post.create(user: first_user, title: 'Hello', text: 'This is my second post')
