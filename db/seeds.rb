f1a7cefd1248999b17a81c821e74effc5c7e1f92
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-02 21:55:34 +0100

    Update the post's show and index methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-user1 = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1492681290082-e932832941e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80', bio: 'Teacher from Mexico.')
-user2 = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', bio: 'Teacher from Poland.')
-user3 = User.create(name: 'John', photo: 'https://images.unsplash.com/photo-1492681290082-e932832941e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80', bio: 'Developer from Germany.')
-user4 = User.create(name: 'Jane', photo: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', bio: 'Singer and mother of two.')
-
-post1 = Post.create(author: user1, title: 'Hello', text: 'This is my first post')
-post2 = Post.create(author: user1, title: 'Hello', text: 'This is my second post')
-post3 = Post.create(author: user2, title: 'Hello', text: 'This is my first post')
-post4 = Post.create(author: user2, title: 'Hello', text: 'This is my second post')
-post5 = Post.create(author: user3, title: 'Hello', text: 'This is my first post')
