528cdcc51f5e4c8e7a96d4f91231d970ca8cd8d9
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-02 22:27:23 +0100

    Create the index view for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:id])
     @post = @user.posts.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,5 @@ Rails.application.routes.draw do
   get '/users/:id/posts/', to: 'posts#index'
   get '/users/:id/posts/:id', to: 'posts#show'
 end
+
+
