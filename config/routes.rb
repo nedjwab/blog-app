eb1886c4fa45334414d5bd22440a73de2f71908b
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-03 11:13:59 +0100

    Add link between the index and the show actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/users/:id', to: 'users#show'
-  get '/users/', to: 'users#index'
-  get '/users/:id/posts/', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
