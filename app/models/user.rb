e8ad44279d31ed1d67eccbe6dcb5faf1d9e57a10
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-08 14:48:37 +0100

    Update user modal and add devise route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, foreign_key: 'user_id', class_name: 'Comment'
   has_many :posts, foreign_key: 'user_id', class_name: 'Post'
   has_many :likes, foreign_key: 'user_id', class_name: 'Like'
