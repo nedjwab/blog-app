8f0bf738383b79c51da805d0ca1b73932144f98c
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-08 14:58:12 +0100

    Add devise to the user modal

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :user
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
