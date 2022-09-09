81048fd0fc9852fb13b11a0563e074874d534103
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-09-09 13:02:36 +0100

    Configure the ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,16 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+  # Define abilities for the user here. For example:
+    #
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Comment, user: user
+    can :manage, Post, user: user
+    return unless user.admin?
+
+    can :manage, :all
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
